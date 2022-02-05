bb730412f5754d0e4c3c2781c73560bb6dbf6454
AlQa-hOlic <rohit.k.kesavan@gmail.com>
2022-02-05 19:18:35 +0530

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/main.css'
+
+function _App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default _App
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return <h1 className="text-3xl font-normal">Hello world!</h1>;
+};
+
+export default Index;
